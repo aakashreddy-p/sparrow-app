16275027a18b4384bab3fa0915c8a40c68ef8945
Aakash Reddy <aakash.reddy@techdome.net.in>
2025-04-01 19:19:34 +0530

    Feat: testflow component block

--- packages/@sparrow-library/src/icons/index.ts
+++ packages/@sparrow-library/src/icons/index.ts
@@ -223,3 +223,4 @@ export { default as WarningFilled } from "./WarningFilled.svelte";
 export { default as DismissCircleRegular } from "./DismissCircleRegular.svelte";
 export { default as InfoFilled } from "./InfoFilled.svelte";
 export { default as OpenRegular } from "./OpenRegular.svelte";
+export { default as RenameRegular } from "./RenameRegular.svelte";
